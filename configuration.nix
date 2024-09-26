b66f7e74e237d50a4a319b4cc43a0ff8a8bae3b5
Kevin Corso <kevrisingdragoon@gmail.com>
2024-09-26 08:10:15 +0000

    VAR: commenti nel conf

--- configuration.nix
+++ configuration.nix
@@ -65,6 +65,10 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  # Enable the GNOME Desktop Environment.
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+
   # Serve per hyprland ma non funziona
   #programs.hyprland.enable = true;
 
