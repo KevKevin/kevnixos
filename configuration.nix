82e441afd4c5656d658d6dd26ebfd9616acc266a
Kevin Corso <kevrisingdragoon@gmail.com>
2024-10-04 07:37:18 +0000

    ADD: chrome per prendere i preferiti

--- configuration.nix
+++ configuration.nix
@@ -251,6 +251,7 @@
   pkgs.wpa_supplicant         #rete
   #pkgs.gnome.gnome-keyring   #roba gnome / i3
   pkgs.firefox                #browser
+  pkgs.google-chrome
   
   pkgs.eww
   pkgs.rofi-wayland # da testare
