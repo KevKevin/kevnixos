12b6c9720d3bf8e1a4facce4accd6cfb20bcb08f
Kevin Corso <kevrisingdragoon@gmail.com>
2024-10-06 20:55:36 +0000

    VAR: bar i3

--- reset_like_remote.sh
+++ reset_like_remote.sh
@@ -1,3 +1,5 @@
+git pull
+
 echo - Creo cartella per la configurazione nix
 mkdir -p /etc/nixos
 
@@ -5,25 +7,25 @@ echo
 echo - Copio la configurazione di nixos: configuration.nix
 sudo cp ~/Documents/kevnixos/configuration.nix /etc/nixos/configuration.nix
 
-echo
-echo - Creo cartella per hypr
-mkdir -p ~/.config/hypr
+# echo
+# echo - Creo cartella per hypr
+# mkdir -p ~/.config/hypr
 
-echo
-echo - Copio la configurazione di Hyprland: hyprland.conf
-sudo cp ~/Documents/kevnixos/hyprland.conf ~/.config/hypr/hyprland.conf
+# echo
+# echo - Copio la configurazione di Hyprland: hyprland.conf
+# sudo cp ~/Documents/kevnixos/hyprland.conf ~/.config/hypr/hyprland.conf
 
-echo
-echo - Copio la configurazione di Hyprpaper: hyprpaper.conf
-sudo cp ~/Documents/kevnixos/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
+# echo
+# echo - Copio la configurazione di Hyprpaper: hyprpaper.conf
+# sudo cp ~/Documents/kevnixos/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
 
-echo
-echo - Creo cartella per waybar
-mkdir -p ~/.config/waybar
+# echo
+# echo - Creo cartella per waybar
+# mkdir -p ~/.config/waybar
 
-echo
-echo - Copio la configurazione di Waybar: config.jsonc
-sudo cp ~/Documents/kevnixos/config.jsonc ~/.config/waybar/config.jsonc
+# echo
+# echo - Copio la configurazione di Waybar: config.jsonc
+# sudo cp ~/Documents/kevnixos/config.jsonc ~/.config/waybar/config.jsonc
 
 echo
 echo - Creo cartella per i3
