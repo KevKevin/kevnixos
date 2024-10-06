13c627028b48626d91b6e837b710ad8bef212141
Kevin Corso <kevrisingdragoon@gmail.com>
2024-10-06 21:07:52 +0000

    VAR: rimossa copia di dati di hyprland dallo script aggiorna

--- aggiorna.sh
+++ aggiorna.sh
@@ -10,26 +10,6 @@ echo
 echo - Copio la configurazione di nixos: configuration.nix
 sudo cp ~/kevnixos/configuration.nix /etc/nixos/configuration.nix
 
-# echo
-# echo - Creo cartella per hypr
-# mkdir -p ~/.config/hypr
-
-# echo
-# echo - Copio la configurazione di Hyprland: hyprland.conf
-# sudo cp ~/kevnixos/hyprland.conf ~/.config/hypr/hyprland.conf
-
-# echo
-# echo - Copio la configurazione di Hyprpaper: hyprpaper.conf
-# sudo cp ~/kevnixos/hyprpaper.conf ~/.config/hypr/hyprpaper.conf
-
-# echo
-# echo - Creo cartella per waybar
-# mkdir -p ~/.config/waybar
-
-# echo
-# echo - Copio la configurazione di Waybar: config.jsonc
-# sudo cp ~/kevnixos/config.jsonc ~/.config/waybar/config.jsonc
-
 echo
 echo - Creo cartella per i3
 mkdir -p ~/.config/i3
