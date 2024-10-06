355fff91964b9c3854b1f596dc0aacd66aafb5f9
Kevin Corso <kevrisingdragoon@gmail.com>
2024-10-06 21:02:32 +0000

    VAR: conf i3

--- aggiorna.sh
+++ aggiorna.sh
@@ -36,12 +36,8 @@ echo - Copio la configurazione di i3: config
 sudo cp ~/Documents/kevnixos/i3config.conf ~/.config/i3/config
 
 echo
-echo - Creo cartella per i3status
-mkdir -p ~/.i3
-
-echo
 echo - Copio la configurazione di i3status: config
-sudo cp ~/Documents/kevnixos/i3statusconfig.conf ~/.i3/i3status.conf
+sudo cp ~/Documents/kevnixos/i3statusconfig.conf ~/.config/i3/i3status.conf
 
 echo
-echo Reset completato!
+echo Aggiornamento completato!
