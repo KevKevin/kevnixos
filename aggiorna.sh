19aa91129abdee4594e71e13a23abecb37a37a49
Kevin Corso <kevrisingdragoon@gmail.com>
2024-10-23 09:33:05 +0000

    VAR: aggiorna senza i3

--- aggiorna.sh
+++ aggiorna.sh
@@ -4,23 +4,23 @@ sudo git pull
 
 echo
 echo - Creo cartella per la configurazione nix
-mkdir -p /etc/nixos
+sudo mkdir -p /etc/nixos
 
 echo
 echo - Copio la configurazione di nixos: configuration.nix
 sudo cp ~/kevnixos/configuration.nix /etc/nixos/configuration.nix
 
-echo
-echo - Creo cartella per i3
-mkdir -p ~/.config/i3
+# echo
+# echo - Creo cartella per i3
+# mkdir -p ~/.config/i3
 
-echo
-echo - Copio la configurazione di i3: config
-sudo cp ~/kevnixos/i3config.conf ~/.config/i3/config
+# echo
+# echo - Copio la configurazione di i3: config
+# sudo cp ~/kevnixos/i3config.conf ~/.config/i3/config
 
-echo
-echo - Copio la configurazione di i3status: config
-sudo cp ~/kevnixos/i3statusconfig.conf ~/.config/i3/i3status.conf
+# echo
+# echo - Copio la configurazione di i3status: config
+# sudo cp ~/kevnixos/i3statusconfig.conf ~/.config/i3/i3status.conf
 
 echo
 echo Aggiornamento completato!
